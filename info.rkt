ea3a3d9fa037b517dc69ee54f21c9be006d39b20
Greg Hendershott <git@greghendershott.com>
2023-03-13 15:52:17 -0400

    Add Makefile and fix info.rkt
    So that `make install` and `make check-deps` succeed.

--- info.rkt
+++ info.rkt
@@ -1,7 +1,12 @@
 #lang info
 
-(define deps '(["base" #:version 8.7]
-               ["drracket-tool-text-lib" #:version 1.3]))
+(define deps '(["base" #:version "8.7"]
+               ["drracket-tool-text-lib" #:version "1.2"] ;want "1.3" when released
+               "sql"
+               "db-lib"
+               "data-lib"
+               "rackunit-lib"))
 (define build-deps '("rackunit-lib"))
+(define compile-omit-paths '("example/"))
 (define test-omit-paths '("example/"))
 (define clean '("compiled"))
